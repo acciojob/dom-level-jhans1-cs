41554ff444455c0bac259d41abfdbe16cb93a724
Jhansi <95856797+jhans1-cs@users.noreply.github.com>
2023-06-25 20:19:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Get the element with id "level"
+var element = document.getElementById("level");
+
+// Start from the element and traverse its parent nodes
+var level = 0;
+var currentNode = element;
+while (currentNode.parentNode !== null) {
+  currentNode = currentNode.parentNode;
+  level++;
+}
+
+// Print the DOM level using an alert
+alert("The level of the element is: " + level);
